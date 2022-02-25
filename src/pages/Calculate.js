059ab8b42e98680ed62f7c5d2ad6cd96a7e02f9a
zairdon20 <josphat2020@gmail.com>
2022-02-25 11:00:02 +0200

    Add the routers>> and Update the index.js file

---
+++ src/pages/Calculate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const Calculate = () => (
+  <>
+    <div className="wrapper">
+      <div>
+        <h2 className="math-text">Let us do some Math!!</h2>
+        <Calculator />
+      </div>
+    </div>
+  </>
+);
+
+export default Calculate;
